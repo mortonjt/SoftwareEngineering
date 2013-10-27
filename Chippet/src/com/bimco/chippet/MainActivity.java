2333c5fb0e470940a19270b2909e48f352938320
Yu Luo <luoy6@miamioh.edu>
2013-10-27 08:36:15 -0400

    Add log, will test if the data time stamp is working

--- Chippet/src/com/bimco/chippet/MainActivity.java
+++ Chippet/src/com/bimco/chippet/MainActivity.java
@@ -1,15 +1,35 @@
 package com.bimco.chippet;
 
+import java.util.List;
+
+import com.bimco.chippet.data.ClipperDataSource;
+import com.bimco.chippet.data.ClipperItem;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.*;
 import android.view.Menu;
 
+/**
+ * This class represent the primary screen
+ */
 public class MainActivity extends Activity {
-
+	
+	private ClipperDataSource dataSource;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        dataSource = new ClipperDataSource();
+        List<ClipperItem> clipperLists = dataSource.findAll();
+        ClipperItem clipper = clipperLists.get(0); 
+        
+        
+        Log.i("CLIPOER", clipper.getKey());
+        
     }
 
 
