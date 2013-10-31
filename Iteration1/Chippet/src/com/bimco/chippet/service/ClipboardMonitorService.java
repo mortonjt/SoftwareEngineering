3866be1a1c1e60b97ebd374360edf8449014beeb
luo-yu <luoy6@miamioh.edu>
2013-10-30 21:44:54 -0400

    Created Clip class

---
+++ Iteration1/Chippet/src/com/bimco/chippet/service/ClipboardMonitorService.java
@@ -0,0 +1,16 @@
+package com.bimco.chippet.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ClipboardMonitorService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
