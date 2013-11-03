86048e429eb4c0e591fe31466aad51c2cd96289a
luo-yu <luoy6@miamioh.edu>
2013-11-03 13:05:33 -0500

    Clipboard monitor service created

--- Iteration1/Chippet/src/com/bimco/chippet/service/ClipboardMonitor.java
+++ Iteration1/Chippet/src/com/bimco/chippet/service/ClipboardMonitor.java
@@ -1,5 +1,15 @@
 package com.bimco.chippet.service;
 
-public class ClipboardMonitor {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ClipboardMonitor extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
