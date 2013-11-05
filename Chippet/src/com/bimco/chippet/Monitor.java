d6eaac3cf5e6160846766102c100fb95ff0bee97
Jamie <jamietmorton@gmail.com>
2013-11-04 20:53:52 -0500

    Adding back in eclipse files

---
+++ Chippet/src/com/bimco/chippet/Monitor.java
@@ -0,0 +1,32 @@
+package com.bimco.chippet;
+import android.app.Service;
+import android.content.ClipboardManager;
+import android.content.ClipboardManager.OnPrimaryClipChangedListener;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Monitor extends Service{
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
