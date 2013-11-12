f7ee50311f996dba366bfc4f1ec8133069f95d57
Jamie <jamietmorton@gmail.com>
2013-11-12 14:44:16 -0500

    Alarm working

--- Chippet/src/com/bimco/chippet/MainActivity.java
+++ Chippet/src/com/bimco/chippet/MainActivity.java
@@ -1,9 +1,14 @@
 package com.bimco.chippet;
+import java.util.Calendar;
+
 import com.bimco.chippet.data.ClipboardTextGetter;
 import com.bimco.chippet.service.ClipboardWatchService;
 
-
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -16,12 +21,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-	//Data structure for all the copied text
-	private ClipboardTextGetter textGetter;
-
-	//private TextView mCopiedText;
+//private TextView mCopiedText;
+	private static final long REPEAT_TIME = 1000 * 1;
 	private Button mCopiedText;
+	private final String SERVICE ="com.bimco.chippet.service.ClipboardWatchService";
 	//private Button mCopiedTextTester;
 	/**
 	 * Basic ideas:
@@ -32,65 +35,40 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		textGetter = new ClipboardTextGetter(this);
-		
-		
-		mCopiedText = (Button)findViewById(R.id.text_copied);		
+		mCopiedText = (Button)findViewById(R.id.text_copied);
 		mCopiedText.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				/*				
-				* ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-				ClipData clipData = clipboard.getPrimaryClip();
-				Item itemAt = clipData.getItemAt(0);
-				String s = itemAt.getText().toString();
- 				*/
-				String s = textGetter.pop();
-				mCopiedText.setText(s);
-				//ClipData clip = ClipData.newPlainText("label", "This is a tester to show the copy function of this button");
-				//clipboard.setPrimaryClip(clip);//?? This will have to be replaced with a proper queue
-				/*Toast = pop up message*/ 
 				Toast.makeText(MainActivity.this,
 						R.string.copied,
 						Toast.LENGTH_SHORT).show();
-
 			}
 		});
+
+		/*AlarmManager service = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		*/
+		Intent i = new Intent(getApplicationContext(), ClipboardWatchService.class);
+		PendingIntent pending = PendingIntent.getBroadcast(getApplicationContext(), 0, i,
+				PendingIntent.FLAG_CANCEL_CURRENT);
+		Calendar cal = Calendar.getInstance();
+		// Start 30 seconds after boot completed
+		cal.add(Calendar.SECOND, 1);
 		
-		this.startService(new Intent(this, ClipboardWatchService.class));		
-		
-		
-		/*
-		 * Runs in the background
-		 * Notification is a service that is run in the notification menu 
-		 */
-/*		final NotificationSetting notificationSetting = new NotificationSetting(this);
-		notificationSetting.act();
-*/
+		// Fetch every second
+		// InexactRepeating allows Android to optimize the energy consumption
+		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+		//am.set(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(), pending);
+		am.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+				cal.getTimeInMillis(), REPEAT_TIME, pending);
 	}
-
-
+	
 	@Override
 	/**
 	 * This occurs when application is in the foreground (aka. not in the background)
 	 */
 	protected void onResume() {
 		super.onResume();
-		
-		/*
-		 * Does this clear out the ClipboardTextGetter (aka. is memory lost)?
-		 */
-		mCopiedText.setText(R.string.hint);
-		/*
-		textGetter = new ClipboardTextGetter(this);
-		if (textGetter.getText() == null) {
-			mCopiedText.setText(R.string.hint);
-		} else {
-			mCopiedText.setText(textGetter.getText());
-		}
-*/	
 	}
 
 
