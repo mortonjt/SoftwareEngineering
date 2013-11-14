c7a2f218dac2eb7df0613fe19b150cbaad26631c
Jamie <jamietmorton@gmail.com>
2013-11-14 09:40:36 -0500

    Added SharedPreferences

--- Chippet/src/com/bimco/chippet/service/ClipboardWatchService.java
+++ Chippet/src/com/bimco/chippet/service/ClipboardWatchService.java
@@ -2,6 +2,8 @@ package com.bimco.chippet.service;
 
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 import com.bimco.chippet.MainActivity;
@@ -14,7 +16,10 @@ import android.content.ClipboardManager;
 import android.content.ClipboardManager.OnPrimaryClipChangedListener;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -32,7 +37,9 @@ public class ClipboardWatchService extends Service {
 	private ClipboardManager mClipboardManager;
 	private OnPrimaryClipChangedListener mClipListener;
 	private Set<String> clipSet;
-    
+	private Queue<String> clipQueue;
+	private SharedPreferences clipPreferences;
+	private int numElements = 3;  //This needs to be initalized by config file 
 	/*
      * (non-Javadoc)
      * @see android.app.Service#onBind(android.content.Intent)
@@ -49,8 +56,37 @@ public class ClipboardWatchService extends Service {
         super.onCreate();
         clipSet = new HashSet<String>(20);
  		mClipboardManager = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+ 		clipQueue = new LinkedList<String>();
+ 		clipPreferences = PreferenceManager.getDefaultSharedPreferences(this);
     }
-    
+    /**
+     * Adds clip to the data structure
+     */
+    public void addClip(String cliptext){
+    	if(!clipSet.contains(cliptext)){
+    		clipSet.add(cliptext);
+    		clipQueue.add(cliptext);
+    		Editor editor = clipPreferences.edit();
+			editor.putString(cliptext, cliptext);
+			Log.e("Adding string",cliptext);
+			if(clipQueue.size()>numElements){
+    			String removed = clipQueue.poll();
+    			clipSet.remove(removed);
+    			editor.remove(removed);
+    			Log.e("Removing string",removed);
+    		}
+    		editor.commit();
+    		
+    	}
+    }
+
+    private void savePreferences(String key, String value) {
+    	SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    	Editor editor = sharedPreferences.edit();
+    	editor.putString(key, value);
+    	editor.commit();	
+    }
+
     @Override
     /**
      * Starts services (aka. checks clipboard every second)
@@ -60,10 +96,8 @@ public class ClipboardWatchService extends Service {
     		if ((mClipboardManager.hasPrimaryClip())) {
     			ClipData.Item item = mClipboardManager.getPrimaryClip().getItemAt(0);
     			String cliptext = item.getText().toString();
-    			if(!clipSet.contains(cliptext)){
-    				clipSet.add(cliptext);
-    				Log.e("Adding string",cliptext);
-    			}
+    			//if(!clipSet.contains(cliptext)){
+    			addClip(cliptext);    			
     		}else{
     			ClipData clip = ClipData.newPlainText("simple text","Hello, World!");
     	        mClipboardManager.setPrimaryClip(clip);
